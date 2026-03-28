41d9fc4d662d2cdac2a889776bbeb9568507eda0
mbranca2 <m.branca2@studenti.unisa.it>
2026-03-28 13:51:51 +0100

    fix db

--- src/main/java/model/ConPool.java
+++ src/main/java/model/ConPool.java
@@ -13,10 +13,10 @@ public class ConPool {
     public static Connection getConnection() throws SQLException {
         if (datasource == null) {
             PoolProperties p = new PoolProperties();
-            p.setUrl("jdbc:mysql://localhost:3306/db_progetto?serverTimezone=" + TimeZone.getDefault().getID());
+            p.setUrl("MY_URL (Da cambiare)" + TimeZone.getDefault().getID());
             p.setDriverClassName("com.mysql.cj.jdbc.Driver");
-            p.setUsername("root");
-            p.setPassword("Mario.luigi23!..");
+            p.setUsername("MY_USERNAME (Da cambiare)");
+            p.setPassword("MY_PASSWORD (Da cambiare)");
             p.setMaxActive(100);
             p.setInitialSize(10);
             p.setMinIdle(10);
